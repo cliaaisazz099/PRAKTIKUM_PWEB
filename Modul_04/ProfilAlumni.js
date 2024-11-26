5f140e48361fb2f8cfd5aaa6fb0cb0830deac26a
cliaaisazz099 <20230910040@uniku.ac.id>
2024-11-26 21:08:04 +0700

    MODUL_04(ALUMNI)

---
+++ Modul_04/ProfilAlumni.js
@@ -0,0 +1,16 @@
+function filterAlumni() {
+    let input = document.getElementById('search-input').value.toLowerCase(); 
+    let alumniItems = document.querySelectorAll('.alumni-item');
+
+    alumniItems.forEach(item => {
+        let name = item.getAttribute('data-name').toLowerCase();
+        let major = item.getAttribute('data-major').toLowerCase(); 
+        const batch = item.getAttribute('data-batch').toLowerCase();
+    
+        if (name.includes(input) || major.includes(input) || batch.includes(input)) {
+            item.style.display = '';  
+        } else {
+            item.style.display = 'none'; 
+        }
+    });
+}
